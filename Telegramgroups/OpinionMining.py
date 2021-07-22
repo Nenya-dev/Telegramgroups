c17ffb256328d38ef9d2d600e92f24f19248fc21
Nenjana <byjuliar@gmail.com>
2021-07-22 15:41:04 +0200

    update dep

--- Telegramgroups/OpinionMining.py
+++ Telegramgroups/OpinionMining.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from spacy.symbols import nsubj, VERB, ADJ, ADV
 from textblob_de import TextBlobDE as tb
 import nltk
 from nltk.corpus import stopwords
@@ -7,15 +8,17 @@ from Telegramgroups import NLP
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 import numpy as np
 from itertools import islice
+
 posts = NLP.posts_prep_two
+messages_original = NLP.posts_prep_one
+nlp = spacy.load("de_core_news_sm")
 
 
 def part_of_speech():
     pos = []
     name_entity = []
 
-    nlp = spacy.load("de_core_news_sm")
-    df = pd.DataFrame(list(posts.find()))
+    df = pd.DataFrame(list(messages_original.find()))
     # df = text_data['Message'].convert_dtypes(convert_string=True)
     for message in df['Message']:
         nlp_message = nlp(message)
@@ -28,9 +31,27 @@ def part_of_speech():
 
         df['entity_text'] = pd.Series(name_entity)
         df['PoS'] = pd.Series(pos)
-    df.to_pickle('PoS.pkl')
+    df.to_pickle('PoS2.pkl')
 
     return df
+
+
+def word_dependency():
+    # df = pd.DataFrame(list(messages_original.find()))
+    df = pd.read_pickle('PoS2.pkl')
+   # print(df['PoS'].head(50))
+    df1 = df.head(100)
+    nlp.add_pipe("merge_entities")
+    nlp.add_pipe("merge_noun_chunks")
+
+    for doc in nlp.pipe(df.Message):
+        for token in doc:
+            print(token.dep_)
+            if token.ent_type_ == "PER" or token.ent_type_ == "ORG":
+                if token.dep_ in ('adc', 'avc'):
+                    print("Token: ", token.text, "--->", "ADJ: ", token)
+                else:
+                    continue
 
 
 def feature_extraction():
@@ -70,82 +91,8 @@ def TFidf():
     print(weights_df.sort_values(by='weight', ascending=False).head(20))
 
 
-def aspect_sentiment_analysis():
-    stopwords_list = stopwords.words("german")
-    fcluster = []
-    totalfeatureList = []
-    finalcluster = []
-    dic = []
-
-    sentList = nltk.sent_tokenize(txt)
-
-    for line in sentList:
-        newtaggedList = []
-        txt_list = nltk.word_tokenize(line)
-        taggedList = nltk.pos_tag(txt_list)
-
-        newwordList = []
-        flag = 0
-        for i in range(0, len(taggedList) - 1):
-            if taggedList[i][1] == 'NN' and taggedList[i + 1][1] == 'NN':
-                newwordList.append(taggedList[i][0] + taggedList[i + 1][0])
-                flag = 1
-            else:
-                if flag == 1:
-                    flag = 0
-                    continue
-                newwordList.append(taggedList[i][0])
-                if i == len(taggedList) - 2:
-                    newwordList.append(taggedList[i + 1][0])
-
-        finaltxt = ' '.join(word for word in newwordList)
-        new_txt_list = nltk.word_tokenize(finaltxt)
-        wordsList = [w for w in new_txt_list if not w in stopwords_list]
-        taggedList_word = nltk.pos_tag(wordsList)
-
-        doc = nlp(finaltxt)  # Object of Stanford NLP Pipeleine
-
-        # Getting the dependency relations betwwen the words
-        dep_node = []
-        for dep_edge in doc.sentences[0].dependencies:
-            dep_node.append([dep_edge[2].text, dep_edge[0].index, dep_edge[1]])
-
-        # Coverting it into appropriate format
-        for i in range(0, len(dep_node)):
-            if int(dep_node[i][1]) != 0:
-                dep_node[i][1] = newwordList[(int(dep_node[i][1]) - 1)]
-
-        featureList = []
-        categories = []
-        for i in taggedList:
-            if i[1] == 'JJ' or i[1] == 'NN' or i[1] == 'JJR' or i[1] == 'NNS' or i[1] == 'RB':
-                featureList.append(list(i))  # For features for each sentence
-                totalfeatureList.append(list(i))  # Stores the features of all the sentences in the text
-                categories.append(i[0])
-
-        for i in featureList:
-            filist = []
-            for j in dep_node:
-                if ((j[0] == i[0] or j[1] == i[0]) and (
-                        j[2] in ["nsubj", "acl:relcl", "obj", "dobj", "agent", "advmod", "amod", "neg", "prep_of",
-                                 "acomp", "xcomp", "compound"])):
-                    if j[0] == i[0]:
-                        filist.append(j[1])
-                    else:
-                        filist.append(j[0])
-            fcluster.append([i[0], filist])
-
-    for i in totalfeatureList:
-        dic[i[0]] = i[1]
-
-    for i in fcluster:
-        if dic[i[0]] == "NN":
-            finalcluster.append(i)
-
-    return finalcluster
-
-
 if __name__ == '__main__':
-   # part_of_speech()
-#   feature_extraction()
-    TFidf()
+    # part_of_speech()
+    #   feature_extraction()
+    # TFidf()
+    word_dependency()
